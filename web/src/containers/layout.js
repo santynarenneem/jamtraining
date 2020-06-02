8667d5ca852d2ffc5eebbb5393e821bd36d7cbee
santynareenneem <narendrasanthosh@neemconsulting.com>
2020-06-02 15:24:24 +0530

    Merge Conflict Changed

--- web/src/containers/layout.js
+++ web/src/containers/layout.js
@@ -6,7 +6,6 @@ const query = graphql`
   query SiteTitleQuery {
     site: sanitySiteSettings(_id: {regex: "/(drafts.|)siteSettings/"}) {
       title
-<<<<<<< HEAD
     },
     sanityNavBar(name: {eq: "Header"}) {
       name
@@ -27,24 +26,12 @@ const query = graphql`
 function LayoutContainer (props) {
   const [showNav, setShowNav] = useState(false)
 
-=======
-    }
-  }
-`
-
-function LayoutContainer (props) {
-  const [showNav, setShowNav] = useState(false)
->>>>>>> c4051dd440968ae4407044379d04672487802a47
   function handleShowNav () {
     setShowNav(true)
   }
   function handleHideNav () {
     setShowNav(false)
   }
-<<<<<<< HEAD
-
-=======
->>>>>>> c4051dd440968ae4407044379d04672487802a47
   return (
     <StaticQuery
       query={query}
@@ -54,34 +41,23 @@ function LayoutContainer (props) {
             'Missing "Site settings". Open the Studio at http://localhost:3333 and some content in "Site settings"'
           )
         }
-<<<<<<< HEAD
         {console.log(data)}
         return (
 
-=======
-        return (
->>>>>>> c4051dd440968ae4407044379d04672487802a47
           <Layout
             {...props}
             showNav={showNav}
             siteTitle={data.site.title}
             onHideNav={handleHideNav}
             onShowNav={handleShowNav}
-<<<<<<< HEAD
             nav = {data.sanityNavBar}
-=======
->>>>>>> c4051dd440968ae4407044379d04672487802a47
           />
         )
       }}
     />
-<<<<<<< HEAD
 
   )
 
-=======
-  )
->>>>>>> c4051dd440968ae4407044379d04672487802a47
 }
 
 export default LayoutContainer
