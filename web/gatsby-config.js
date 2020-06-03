e69bccc6dfc514967a1f7460da2ab55420721b05
santynareenneem <narendrasanthosh@neemconsulting.com>
2020-06-03 11:17:10 +0530

    PWA Manifest added

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -20,6 +20,24 @@ module.exports = {
         watchMode: !isProd,
         overlayDrafts: !isProd
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "NASA ATH",
+        short_name: "ATH",
+        start_url: "/home",
+        background_color: "#0099D9",
+        theme_color: "#0099D9",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/assets/img/appicon.webp", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
   ]
 }
---
+++ web/src/components/tileSlides.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function tileSlides() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default tileSlides
