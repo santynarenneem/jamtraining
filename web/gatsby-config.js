4a1fdf77bc915419028074e0bb9764f27c1071b7
santynareenneem <narendrasanthosh@neemconsulting.com>
2020-06-03 11:37:34 +0530

    maskable

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -32,15 +32,7 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: "standalone",
-        icon: [
-          {
-            "src": "src/assets/img/appicon.webp",
-            "sizes": "196x196",
-            "type": "image/webp",
-            "purpose": "any maskable"
-          }
-        ],
-         // This path is relative to the root of the site.
+        icon: "src/assets/img/appicon.webp", // This path is relative to the root of the site.
         // An optional attribute which provides support for CORS check.
         // If you do not provide a crossOrigin option, it will skip CORS for manifest.
         // Any invalid keyword or empty string defaults to `anonymous`
